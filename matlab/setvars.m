f540cc270991af601e695f150036b69041fc8c79
Antyradek <antyradek@gmail.com>
2016-12-07 00:28:17 +0100

    Zrób zadanie 4

--- matlab/setvars.m
+++ matlab/setvars.m
@@ -10,3 +10,11 @@ Ad2 = Ad';
 Bd2 = Cd';
 Cd2 = Bd';
 Dd2 = Dd;
+
+%3
+syms z;
+trans1 = Cd * inv(z * eye(3) - Ad) * Bd + Dd;
+trans2 = Cd2 * inv(z * eye(3) -Ad2) * Bd2 + Dd2;
+isequal(trans1,trans2);
+trans1 = simplify(trans1);
+trans2 = simplify(trans2);
