2835c648abfa727ff4f9be1cebd6c43fee0deea4
Antyradek <antyradek@gmail.com>
2016-12-11 03:03:01 +0100

    Zrób zadanie 6

--- matlab/setvars.m
+++ matlab/setvars.m
@@ -5,15 +5,15 @@ mian = [1 7 -14 -120];
 [liczdys,miandys] = c2dm(licz,mian,T,'zoh');
 
 %2
-[Ad,Bd,Cd,Dd] = tf2ss(liczdys,miandys);
-Ad2 = Ad';
-Bd2 = Cd';
-Cd2 = Bd';
-Dd2 = Dd;
+[Ad1,Bd1,Cd1,Dd1] = tf2ss(liczdys,miandys);
+Ad2 = Ad1';
+Bd2 = Cd1';
+Cd2 = Bd1';
+Dd2 = Dd1;
 
 %3
 syms z;
-trans1 = Cd * inv(z * eye(3) - Ad) * Bd + Dd;
+trans1 = Cd1 * inv(z * eye(3) - Ad1) * Bd1 + Dd1;
 trans2 = Cd2 * inv(z * eye(3) -Ad2) * Bd2 + Dd2;
 isequal(trans1,trans2);
 trans1 = simplify(trans1);
@@ -23,4 +23,12 @@ trans2 = simplify(trans2);
 
 %5
 X0 = [3; -2; 1];
-Kd = acker(Ad2,Bd2,[0 0.2 0.3]);
+%Ustalenie symulowanego obiektu
+Ad = Ad2;
+Bd = Bd2;
+Cd = Cd2;
+Dd = Dd2;
+Kd = acker(Ad,Bd,[0 0.2 0.3]);
+
+%6
+Ld = acker(Ad',Cd',[0 0 0])';
